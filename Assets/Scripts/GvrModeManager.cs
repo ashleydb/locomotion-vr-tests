71cabfccf41448930c70e951660fd41c0c49e826
ashleydb <abennett@gmail.com>
2016-08-03 21:57:24 -0700

    Initial commit
    Basic movement to a marker, based on gaze input.
    Note that some of the scripts included are superfluous, a little hacked
    together.

---
+++ Assets/Scripts/GvrModeManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class GvrModeManager : MonoBehaviour {
+
+	// We need a reference to the gvr components
+	public GameObject gvrViewerMain = null;
+	public Camera mainCamera = null;
+
+	// This is to enable/disable the reticule
+	private GvrReticle gvrReticle = null;
+	// This is to access the gvr SDK functions
+	private GvrViewer gvrViewer = null;
+
+	public void Start()
+	{
+		if (gvrViewerMain != null) {
+			gvrViewer = gvrViewerMain.GetComponentInChildren<GvrViewer> ();
+			if (gvrViewer != null) {
+				// Save a flag in the local player preferences to initialize VR mode
+				// This way when the app is restarted, it is in the mode that was last used.
+				int doVR = PlayerPrefs.GetInt ("VREnabled");
+				gvrViewer.VRModeEnabled = (doVR == 1);
+				gvrViewer.enabled = gvrViewer.VRModeEnabled;
+
+				gvrReticle = mainCamera.GetComponentInChildren<GvrReticle> ();
+				if (gvrReticle != null) {
+					gvrReticle.gameObject.SetActive (gvrViewer.VRModeEnabled);
+				}
+			}
+		}
+	}
+
+	// The event handler to call to toggle gvr mode.
+	public void ChangeVrMode()
+	{
+		if (gvrViewer != null) {
+			if (gvrViewer.VRModeEnabled) {
+				// disabling.  rotate back to the original rotation.
+				mainCamera.transform.localRotation = Quaternion.identity;
+			}
+			gvrViewer.VRModeEnabled = !gvrViewer.VRModeEnabled;
+			gvrViewer.enabled = gvrViewer.VRModeEnabled;
+			gvrReticle.gameObject.SetActive (gvrViewer.VRModeEnabled);
+			PlayerPrefs.SetInt ("VREnabled", gvrViewer.VRModeEnabled ? 1 : 0);
+			PlayerPrefs.Save ();
+		}
+	}
+
+	// The gvr SDK can show a back button, (escaspe key on keyboard,) which we will use to get out of VR view
+	void Update () {
+		if (gvrViewer.BackButtonPressed) {
+			ChangeVrMode();
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public Rigidbody rb;	// Reference to the physics component of this object
+	public float speed;		// Public so we can set this in the inspector in the Unity Editor. 10 is good.
+	private Vector3 moveTowardsPosition; // Position that could be passed in from Gaze, Touch, Mouse to move the player towards
+	private bool shouldMove; // Only move after a player clicks/taps, not each frame
+
+	public GameObject viewMarker; // Object representing the view marker to show in the world
+	public float maxRange = 0.5f; // What we consider close enough to be considered at the view marker when moving
+
+	// Called on first frame this script is active, (i.e. first frame of game)
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+		shouldMove = false;
+	}
+
+	// Happens just before Physics calculations
+	void FixedUpdate() {
+		// If we got input from clicks, taps or cardboard, move towards it. Otherwise, see if there is keyboard/gamepad input.
+		if (shouldMove) {
+			// Get a vector from the player position to the point passed in
+			Vector3 movement = moveTowardsPosition - gameObject.transform.position;
+			// Ignore the y movement
+			movement.y = 0; //gameObject.transform.position.y;
+
+			// Stop any current movement
+			rb.velocity = Vector3.zero;
+
+			// Multiply the input vector by the object's speed, then apply that result as a force to the object to move it
+			rb.AddForce (movement * speed);
+
+			// Don't do this every frame, only when we've been sent a new position, (e.g. OnClick)
+			shouldMove = false;
+		} else {
+			// If the player is at the marker, stop them moving
+			Vector3 heading = moveTowardsPosition - gameObject.transform.position;
+			heading.y = 0;
+			if (heading.sqrMagnitude < maxRange * maxRange) {
+				// Player is pretty much at the view marker. Stop them moving.
+				rb.velocity = Vector3.zero;
+			}
+		}
+	}
+
+	public void SetMoveTowardsPoint(Vector3 worldPosition) {
+		moveTowardsPosition = worldPosition;
+		shouldMove = true;
+	}
+}
