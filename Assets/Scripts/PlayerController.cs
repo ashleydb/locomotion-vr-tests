8f6c0247a799552a005210b4ad12fa05840d9fe0
ashleydb <abennett@gmail.com>
2016-08-11 22:28:57 -0700

    Adding Teleport Mode
    - Added a UI on the ceiling to switch movement modes.
    - Added Teleport as a movement mode, (which is actually a fast warp
    style move, rather than an instant jump.)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,12 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+	// We'll offer multiple ways to move in this demo, so need to switch between them
+	private enum LocomotionMode { LOCO_VIEW_MARKER = 0, LOCO_TELEPORT, ERROR };
+	private LocomotionMode locomotionMode = LocomotionMode.LOCO_VIEW_MARKER;
+
     // Only move after a player clicks/taps, not each frame
-    enum MoveState { SET_VIEW_MARKER = 0, SET_MOVE_POSITION, START_MOVE, MOVING, ERROR };
+    private enum MoveState { SET_VIEW_MARKER = 0, SET_MOVE_POSITION, START_MOVE, MOVING, ERROR };
 	private MoveState moveState = MoveState.SET_VIEW_MARKER;
 
 	public Rigidbody rb;	// Reference to the physics component of this object
@@ -23,13 +27,23 @@ public class PlayerController : MonoBehaviour {
 
 	// Happens just before Physics calculations
 	void FixedUpdate() {
+		switch (locomotionMode) {
+		case LocomotionMode.LOCO_VIEW_MARKER:
+			viewMarkerUpdate ();
+			break;
+		case LocomotionMode.LOCO_TELEPORT:
+			teleportUpdate ();
+			break;
+		default:
+			Debug.Log ("PlayerController::FixedUpdate() - Invalid mode");
+			break;
+		}
+	}
+
+	// Move the player if i LOCO_VIEW_MARKER mode
+	void viewMarkerUpdate() {
 		// If we got input from clicks, taps or cardboard, move towards it. Otherwise, see if there is keyboard/gamepad input.
 		switch (moveState) {
-		case MoveState.SET_VIEW_MARKER:
-			break;
-		case MoveState.SET_MOVE_POSITION:
-			break;
-
 		case MoveState.START_MOVE:
 			// Get a vector from the player position to the point passed in
 			Vector3 movement = moveTowardsPosition - gameObject.transform.position;
@@ -54,16 +68,51 @@ public class PlayerController : MonoBehaviour {
 				// Player is pretty much at the view marker. Stop them moving.
 				rb.velocity = Vector3.zero;
 
-                    // Rotate to look at the view marker, (potentially lerp this in START_MOVE)
-                    Vector3 lookAtPosition = viewMarker.transform.position;
-                    lookAtPosition.y = gameObject.transform.position.y;
+				// Rotate to look at the view marker, (potentially lerp this in START_MOVE)
+				Vector3 lookAtPosition = viewMarker.transform.position;
+				lookAtPosition.y = gameObject.transform.position.y;
 
-                gameObject.transform.LookAt (lookAtPosition);
+				gameObject.transform.LookAt (lookAtPosition);
 
 				moveState = MoveState.SET_VIEW_MARKER;
 
-                    // TOOD: Instead of immediately hiding this, make it fade out after a few seconds to help reorient the player?
+				// TOOD: Instead of immediately hiding this, make it fade out after a few seconds to help reorient the player?
 				//viewMarker.SetActive (false);
+			}
+			break;
+		}
+	}
+
+	// Move the player if i LOCO_TELEPORT mode.
+	// Pretty much the same as viewMarkerUpdate() but simpler MoveState.MOVING code.
+	void teleportUpdate() {
+		// If we got input from clicks, taps or cardboard, move towards it. Otherwise, see if there is keyboard/gamepad input.
+		switch (moveState) {
+		case MoveState.START_MOVE:
+			// Get a vector from the player position to the point passed in
+			Vector3 movement = moveTowardsPosition - gameObject.transform.position;
+			// Ignore the y movement
+			movement.y = 0;
+
+			// Stop any current movement
+			rb.velocity = Vector3.zero;
+
+			// Multiply the input vector by the object's speed, then apply that result as a force to the object to move it
+			rb.AddForce (movement * speed);
+
+			// Don't do this every frame, only when we've been sent a new position, (e.g. OnClick)
+			moveState = MoveState.MOVING;
+			break;
+
+		case MoveState.MOVING:
+			// If the player is at the marker, stop them moving
+			Vector3 heading = moveTowardsPosition - gameObject.transform.position;
+			heading.y = 0;
+			if (heading.sqrMagnitude < maxRange * maxRange) {
+				// Player is pretty much at the view marker. Stop them moving.
+				rb.velocity = Vector3.zero;
+
+				moveState = MoveState.SET_MOVE_POSITION;
 			}
 			break;
 		}
@@ -84,5 +133,36 @@ public class PlayerController : MonoBehaviour {
 			break;
 		}
 
+	}
+
+	public void ChangeLocomotionMode() {
+		++locomotionMode;
+
+		switch (locomotionMode) {
+		case LocomotionMode.ERROR:
+			locomotionMode = LocomotionMode.LOCO_VIEW_MARKER;
+			moveState = MoveState.SET_VIEW_MARKER;
+			break;
+		case LocomotionMode.LOCO_VIEW_MARKER:
+			moveState = MoveState.SET_VIEW_MARKER;
+			break;
+		case LocomotionMode.LOCO_TELEPORT:
+			moveState = MoveState.SET_MOVE_POSITION;
+			break;
+		}
+
+		// Stop the player moving.
+		rb.velocity = Vector3.zero;
+	}
+
+	public string GetLocomotionModeString() {
+		switch (locomotionMode) {
+		case LocomotionMode.LOCO_VIEW_MARKER:
+			return "View Marker";
+		case LocomotionMode.LOCO_TELEPORT:
+			return "Teleport";
+		default:
+			return "Error";
+		}
 	}
 }
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour {
+
+	public PlayerController player;
+	public Text modeText;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	public void ModeButtonClick() {
+		player.ChangeLocomotionMode ();
+		modeText.text = player.GetLocomotionModeString();
+	}
+}
